b267db97356037a4b357b45cd73b4f1f149ee226
supriya <supriyajha2108@gmail.com>
2023-11-18 15:43:57 +0530

    cors updated

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express=require("express");
 const app=express();
+const cors = require('cors');
 const connectDB=require('./db/connect');
 const posts_routes=require("./routes/products");
 const PORT= process.env.PORT || 5000;
